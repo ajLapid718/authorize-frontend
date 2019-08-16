ed2a7357799d9112fbfe10996b19d6d462cb0db4
ajLapid718 <ajLapid718@gmail.com>
2019-08-16 00:05:06 -0400

    Set up auth form container

---
+++ src/components/containers/AuthFormContainer.jsx
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { auth } from "../../thunks";
+import { AuthFormView } from "../views";
+
+// Smart container;
+class AuthFormContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: ""
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const formName = event.target.name;
+    this.props.loginOrSignup(this.state.email, this.state.password, formName);
+  }
+
+  render() {
+    return (
+      <AuthFormView
+        name={this.props.name}
+        displayName={this.props.displayName}
+        error={this.props.error}
+        handleChange={this.handleChange}
+        handleSubmit={this.handleSubmit}
+      />
+    );
+  }
+};
+
+// Map state to props;
+const mapLogin = state => {
+  return {
+    name: "login",
+    displayName: "Login",
+    error: state.user.error
+  };
+};
+
+// Map state to props;
+const mapSignup = state => {
+  return {
+    name: "signup",
+    displayName: "Sign Up",
+    error: state.user.error
+  };
+};
+
+// Map dispatch to props;
+const mapDispatch = dispatch => {
+  return {
+    loginOrSignup: (email, password, formName) => dispatch(auth(email, password, formName))
+  }
+};
+
+export const Login = connect(mapLogin, mapDispatch)(AuthFormContainer);
+export const Signup = connect(mapSignup, mapDispatch)(AuthFormContainer);
--- src/components/containers/index.js
+++ src/components/containers/index.js
@@ -1,2 +1,3 @@
 // Barrel file for container components;
 export { default as AllBooksContainer } from "./AllBooksContainer";
+export { default as AuthFormContainer } from "./AuthFormContainer";
