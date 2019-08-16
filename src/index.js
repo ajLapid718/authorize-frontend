be6029a99c3172b454a04a784d56bfbc02b7e8fa
ajLapid718 <ajLapid718@gmail.com>
2019-08-15 23:20:00 -0400

    Install redux-devtools-extension module and import store from proper location

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import { Provider } from 'react-redux';
-import { store } from './store';
+import store from './store';
 
 ReactDOM.render(
   <Provider store={store}>
