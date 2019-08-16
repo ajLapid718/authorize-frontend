05813aa24941bbd00eb5d6917fb2c907fad07832
ajLapid718 <ajLapid718@gmail.com>
2019-08-16 00:19:37 -0400

    Configure routes container to be subscribed to router changes and to load initial user for entire SPA

--- src/components/routes/RoutesContainer.jsx
+++ src/components/routes/RoutesContainer.jsx
@@ -1,10 +1,28 @@
 import React, { Component } from "react";
 import RoutesView from "./RoutesView";
+import { withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import { me } from "../../thunks";
 
 class RoutesContainer extends Component {
+  componentDidMount() {
+    this.props.loadInitialData();
+  }
   render() {
-    return <RoutesView />
+    return <RoutesView isLoggedIn={this.props.isLoggedIn} />
   }
 }
 
-export default RoutesContainer;
+const mapState = (state) => {
+  return {
+    isLoggedIn: !!state.user.id
+  }
+}
+
+const mapDispatch = (dispatch) => {
+  return {
+    loadInitialData: () => dispatch(me())
+  }
+}
+
+export default withRouter(connect(mapState, mapDispatch)(RoutesContainer));
