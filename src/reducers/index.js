116250a5f445bcd347a95da66b1d3f2d4b21a6bb
ajLapid718 <ajLapid718@gmail.com>
2019-08-15 23:52:05 -0400

    Add the user key to the store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,4 @@
 // A barrel file for our reducers, which will be combined and passed into the Redux store we create;
 // The aliases of reducers in this file will be assigned as the names of the keys in the Redux store, with the values being the respective individual reducers;
 export { default as books } from '../store/utilities/books';
+export { default as user } from '../store/utilities/user';
